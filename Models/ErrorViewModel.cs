6ab64e6d59bda4fee5442224cb10974b07782d82
Georgiana Pop <georgianapop6@yahoo.com>
2023-10-23 14:20:52 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pop_Andreea_Georgiana_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
