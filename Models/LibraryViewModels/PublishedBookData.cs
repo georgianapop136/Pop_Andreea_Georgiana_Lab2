213e00a278b84232e41f04830647347232178708
Georgiana Pop <georgianapop6@yahoo.com>
2023-10-27 15:21:30 +0300

    Lab7

---
+++ Models/LibraryViewModels/PublishedBookData.cs
@@ -0,0 +1,10 @@
+﻿namespace Pop_Andreea_Georgiana_Lab2.Models.LibraryViewModels
+{
+    public class PublishedBookData
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public bool IsPublished { get; set; }
+
+    }
+}
