a6355e6277eaa4a2181e41f1388be7fee3c5c5ba
Georgiana Pop <georgianapop6@yahoo.com>
2024-01-18 00:59:19 +0200

    lab11

---
+++ Controllers/CustomersGrpcController.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Net.Client;
+using GrpcCustomersService;
+using Pop_Andreea_Georgiana_Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pop_Andreea_Georgiana_Lab2.Controllers
+{
+
+    public class CustomersGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public CustomersGrpcController()
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:7021");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(GrpcCustomersService.Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new
+               CustomerService.CustomerServiceClient(channel);
+                var createdCustomer = client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+    }
+}
